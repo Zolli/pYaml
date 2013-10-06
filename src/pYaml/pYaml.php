4dc7bab42e5b91b12948b49ac6b8b9d2de1c3c5d
Zolli <zolli07@gmail.com>
2013-10-06 12:29:06 +0200

    *Implemented save() and set() method in pYaml class, now able to modify or create new YAML files

--- src/pYaml/pYaml.php
+++ src/pYaml/pYaml.php
@@ -1,50 +1,35 @@
 <?php
 namespace pYaml;
 
+use Symfony\Component\Yaml\Dumper as Dumper;
 use \Symfony\Component\Yaml\Yaml as YAML;
 
-/**
- * 
- */
 class pYaml {
     
     /**
-     * Construct data holder
-     * @var pYaml instance
-     */
-    private static $instance = null;
-    
-    /**
      * Contains the parsed result of the YAML file
-     * @var array Egy asszociatív tömb
+     * @var array Assoc array
      */
     public $parseResult = null;
-    
+
     /**
-     * Singletin getter
-     * @return pYaml instance
+     * Holds the parsed file name
+     * @var string The input file name
      */
-    public static function getInstance() {
-        if(!self::$instance) {
-            self::$instance = new pYaml();
-        }
-        
-        return self::$instance;
-    }
-    
+    public $fileName = null;
+
     /**
      * Reads the given file and, parse this
      * with Symfony Yaml component
-     * 
      * @param string $file The file to be parsed
-     * @return array Assoc array
      * @throws Exception If the file is not provided
      */
-    public function init($file = null) {
+    public function __construct($file = null) {
         if($file == null) {
             throw new Exception("File not found " . $file, 404);
         }
-        
+
+        $this->fileName = $file;
         $stream = fopen($file, "r");
         $content = fread($stream, filesize($file));
         $this->parseResult = YAML::parse($content);
@@ -52,24 +37,64 @@ class pYaml {
     
     /**
      * Returns a pYamlSection by the path tree
-     * 
-     * @param \pYaml\nodeSelector\nodeSelector  $path The node location
+     * @param nodeSelector\nodeSelector $path The node location
      * @return pYamlSection
      */
-    public function get(\pYaml\nodeSelector\nodeSelector $selector) {
+    public function get(nodeSelector\nodeSelector $selector) {
         $selector->setParsedYaml($this->parseResult);
         $result = $selector->getNode();
         $sectionParent = $selector->getParentName();
-        return new pYamlSection($result, $sectionParent);
+        return new pYamlSection($result[0], $sectionParent);
+    }
+
+    /**
+     * Set a key in parsed array by the passed selector
+     * @param string $selector Selector string
+     * @param mixed $value The key value
+     * @return \pYaml\pYaml
+     */
+    public function set($selector, $value) {
+        $selectorParts = explode(".", $selector);
+        $current = &$this->parseResult;
+        foreach($selectorParts as $selectorPart) {
+            $current = &$current[$selectorPart];
+        }
+        $current = $value;
+
+        return $this;
+    }
+
+    /**
+     * Save the file array content in yaml format
+     * Must be call after finishing with set() methods
+     * @param null $saveFile If the file is null overwrite the existing file, if its provided create a new file
+     */
+    public function save($saveFile = null) {
+        if($saveFile == null) {
+            $outFile = $this->getFilename();
+        } else {
+            $outFile = $saveFile;
+        }
+
+        $yamlDumper = new Dumper();
+        $fileContent = $yamlDumper->dump($this->getRawResult(), 100, 0, false, true);
+        file_put_contents($outFile, $fileContent);
     }
     
     /**
      * Returns the result from Symfony's Yaml parser component
-     * 
      * @return array Assoc result array
      */
     public function getRawResult() {
         return $this->parseResult;
+    }
+
+    /**
+     * Returns the parsed file name
+     * @return string File name
+     */
+    public function getFilename() {
+        return (string) $this->fileName;
     }
     
 }
